57911fcadf16fee0acc834707ced2fe121978c8b
nisol <nisolmedinaue@gmail.com>
2024-04-10 16:03:58 +0200

    ejercicio a completar

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
 
+
 // Concatenate the string variables into one new string
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4;
 
 
 // Print out the concatenated string
+console.log(tongueTwister);
 
 
 
@@ -22,10 +25,15 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const result = part1[ part1.length -1].toUpperCase();
+const pice = part1.slice( 0, 3);
+
+//var str2 = str1.slice(0, -1) + '.';
+
 
 
 // Print the cameLtaiL-formatted string
-
+console.log(pice);
 
 
 
